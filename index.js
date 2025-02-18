c0b86a2b3929e07d75e013e30ebaea0c600da269
Ami <143962821+SleepingAmi@users.noreply.github.com>
2025-02-18 20:34:36 +0100

    CHORE: Cookies + Cherry-picking Issues
    Changelog (?):
    - Cookie banners (+ css) + cookie acceptance
    - cookie-parser middleware
    - Some more CSS polishing on /upload (+ mimetype list + mimetype list in API error response)
    - /api/accept-cookies route, which stores a cookie saying you accepted cookies for a whole year (I think)
    - Added the cookie modal to 404, index, posts, settings, upload, user and welcome pages
    - Changed from 'private beta' to public beta (we transitioned a while back)

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const { sanitizeText } = require('./middleware/forceTextDirections');
 const sanitizeHtml = require('sanitize-html');
 const fs = require('fs');
 const globals = JSON.parse(fs.readFileSync('global-variables.json', 'utf8'));
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const db = new sqlite3.Database('./database.db');
@@ -82,6 +83,15 @@ app.use(session({
     },
     rolling: true                       // Refresh session with each request
 }));
+app.use(cookieParser());
+
+function checkCookies(req, res) {
+    if (!req.cookies.cookiesAccepted) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 function sanitizeContent(text) {
     const returnable =  sanitizeHtml(text, {
@@ -106,6 +116,7 @@ app.use('/api', apiRoutes);
 
 // FEEEEEEEEEED
 app.get('/', async (req, res) => {
+
     try {
         db.all(`
             SELECT posts.*, users.username, users.pfp
@@ -121,6 +132,7 @@ app.get('/', async (req, res) => {
                     errorMessage: "Method not Allowed.",
                     username: req.session.user?.username || null,
                     ownId: req.session.user?.id || null,
+                    cookies: checkCookies(req)
                 });
             }
 
@@ -136,6 +148,7 @@ app.get('/', async (req, res) => {
                 isAdmin: req.session.user?.isAdmin || null,
                 ownId: req.session.user?.id || null,
                 posts: sanitizedPosts, // Each row includes sanitized posts and user info
+                cookies: checkCookies(req)
             });
         });
 
@@ -145,6 +158,7 @@ app.get('/', async (req, res) => {
             errorMessage: "Unable to update Unmodified Data.",
             username: req.session.user?.username || null,
             ownId: req.session.user?.id || null,
+            cookies: checkCookies(req)
         });
     }
 })
@@ -152,7 +166,8 @@ app.get('/', async (req, res) => {
 // Welpum
 app.get('/welcome', (req, res) => {
     res.render('pages/welcome', {
-        version
+        version,
+        cookies: checkCookies(req)
     })
 })
 app.get('/404', (req, res) => {
@@ -160,7 +175,8 @@ app.get('/404', (req, res) => {
         hydrauliscECode: "85",
         errorMessage: "The requested resource was not found, the system took too long to respond, the system is offline, or you do not have access to view the requested resource.",
         username: req.session.user?.username || null,
-        ownId: req.session.user?.id || null
+        ownId: req.session.user?.id || null,
+        cookies: checkCookies(req)
     });
 })
 
@@ -184,7 +200,8 @@ app.get('/newlyregistered', (req, res) => {
             res.render('partials/registerSuccess', {
                 newUser: pageUser.username + "#" + pageUser.discriminator,
                 username: req.session.user?.username || null,
-                ownId: req.session.user?.id || null
+                ownId: req.session.user?.id || null,
+                cookies: checkCookies(req)
             })
         })
     } else {
@@ -208,7 +225,8 @@ app.get('/user/:id?', (req, res) => {
                 hydrauliscECode: "82",
                 errorMessage: "Undefined.",
                 username: req.session.user?.username || null,
-                ownId: req.session.user?.id || null
+                ownId: req.session.user?.id || null,
+                cookies: checkCookies(req)
             });
         }
 
@@ -223,7 +241,8 @@ app.get('/user/:id?', (req, res) => {
                                 hydrauliscECode: "92",
                                 errorMessage: "Session Not Found/Already Updated.",
                                 username: req.session.user?.username || null,
-                                ownId: req.session.user?.id || null
+                                ownId: req.session.user?.id || null,
+                                cookies: checkCookies(req)
                             });
                         }
 
@@ -240,7 +259,8 @@ app.get('/user/:id?', (req, res) => {
                             uploads: posts,
                             isAdmin: req.session.user?.isAdmin || null,
                             banner: pageUser.banner,
-                            discriminator: pageUser.discriminator || '0000'
+                            discriminator: pageUser.discriminator || '0000',
+                            cookies: checkCookies(req)
                         });
                     }
                 );
@@ -250,7 +270,8 @@ app.get('/user/:id?', (req, res) => {
                 hydrauliscECode: "85",
                 errorMessage: "The requested resource was not found, the system took too long to respond, the system is offline, or you do not have access to view the requested resource.",
                 username: req.session.user?.username || null,
-                ownId: req.session.user?.id || null
+                ownId: req.session.user?.id || null,
+                cookies: checkCookies(req)
             });
         }
     });
@@ -265,7 +286,8 @@ app.get('/post/:id?', (req, res) => {
             hydrauliscECode: "85",
             errorMessage: "The requested resource was not found, the system took too long to respond, the system is offline, or you do not have access to view the requested resource.",
             username: req.session.user?.username || null,
-            ownId: req.session.user?.id || null
+            ownId: req.session.user?.id || null,
+            cookies: checkCookies(req)
         });
     }
 
@@ -276,7 +298,8 @@ app.get('/post/:id?', (req, res) => {
                 hydrauliscECode: "82",
                 errorMessage: "Undefined.",
                 username: req.session.user?.username || null,
-                ownId: req.session.user?.id || null
+                ownId: req.session.user?.id || null,
+                cookies: checkCookies(req)
             });
         }
 
@@ -285,7 +308,8 @@ app.get('/post/:id?', (req, res) => {
                 hydrauliscECode: "85",
                 errorMessage: "The requested resource was not found, the system took too long to respond, the system is offline, or you do not have access to view the requested resource.",
                 username: req.session.user?.username || null,
-                ownId: req.session.user?.id || null
+                ownId: req.session.user?.id || null,
+                cookies: checkCookies(req)
             });
         }
 
@@ -303,7 +327,8 @@ app.get('/post/:id?', (req, res) => {
                     hydrauliscECode: "82",
                     errorMessage: "Undefined.",
                     username: req.session.user?.username || null,
-                    ownId: req.session.user?.id || null
+                    ownId: req.session.user?.id || null,
+                    cookies: checkCookies(req)
                 });
             }
 
@@ -312,7 +337,8 @@ app.get('/post/:id?', (req, res) => {
                     hydrauliscECode: "96",
                     errorMessage: "Account Deleted/Suspended.",
                     username: req.session.user?.username || null,
-                    ownId: req.session.user?.id || null
+                    ownId: req.session.user?.id || null,
+                    cookies: checkCookies(req)
                 });
             }
 
@@ -327,7 +353,8 @@ app.get('/post/:id?', (req, res) => {
                 username: req.session.user?.username || null,
                 isAdmin: req.session.user?.isAdmin || null,
                 post: sanitizedPost, // post.data
-                author: sanitizedAuthor // author.data
+                author: sanitizedAuthor, // author.data
+                cookies: checkCookies(req)
             });
         });
     });
@@ -344,7 +371,8 @@ app.get('/settings', (req, res) => {
                     hydrauliscECode: "92",
                     errorMessage: "Session Not Found/Already Updated.",
                     username: null,
-                    ownId: req.session.user.id
+                    ownId: req.session.user.id,
+                    cookies: checkCookies(req)
                 });
             }
 
@@ -364,7 +392,8 @@ app.get('/settings', (req, res) => {
                         pfp: userDetail.pfp,
                         users,
                         banner: userDetail.banner,
-                        discriminator: userDetail.discriminator || '0000'
+                        discriminator: userDetail.discriminator || '0000',
+                        cookies: checkCookies(req)
                     })
                 });
             } else {
@@ -377,7 +406,8 @@ app.get('/settings', (req, res) => {
                     pfp: userDetail.pfp,
                     users: [],
                     banner: userDetail.banner,
-                    discriminator: userDetail.discriminator || '0000'
+                    discriminator: userDetail.discriminator || '0000',
+                    cookies: checkCookies(req)
                 })
             }
         });
@@ -396,44 +426,28 @@ app.get('/upload', (req, res) => {
                     hydrauliscECode: "92",
                     errorMessage: "Session Not Found/Already Updated.",
                     username: null,
-                    ownId: req.session.user.id
+                    ownId: req.session.user.id,
+                    cookies: checkCookies(req)
                 });
             }
 
-            if(req.session.user.isAdmin) {
-                db.all('SELECT id, username, isAdmin FROM users ORDER BY id', [], (err, users) => {
-                    if (err) {
-                        logError('Failed to load users', err);
-                        return res.redirect('/');
-                    }
-            
-                    res.render('pages/upload', {
-                        isAdmin: userDetail.isAdmin,
-                        username: userDetail.username,
-                        usersBiography: userDetail.biography,
-                        ownId: userDetail.id,
-                        version,
-                        pfp: userDetail.pfp,
-                        users
-                    })
-                });
-            } else {
-                res.render('pages/upload', {
-                    isAdmin: userDetail.isAdmin,
-                    username: userDetail.username,
-                    usersBiography: userDetail.biography,
-                    ownId: userDetail.id,
-                    version,
-                    pfp: userDetail.pfp
-                })
-            }
+            res.render('pages/upload', {
+                isAdmin: userDetail.isAdmin,
+                username: userDetail.username,
+                usersBiography: userDetail.biography,
+                ownId: userDetail.id,
+                version,
+                pfp: userDetail.pfp,
+                cookies: checkCookies(req)
+            })
         });
     } else {
         res.render('pages/404', {
             hydrauliscECode: "89",
             errorMessage: "Method not Allowed.",
             username: req.session.user?.username || null,
-            ownId: req.session.user?.id || null
+            ownId: req.session.user?.id || null,
+            cookies: checkCookies(req)
         });;
     }
 })
